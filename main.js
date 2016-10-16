a2765c8b726a999737fe333ee3857f7b6451e094
Peter Svedman <peter.svedman@gmail.com>
2016-10-16 13:15:21 +0200

    Working JSON calls

--- main.js
+++ main.js
@@ -1,77 +1,13 @@
-// a winky is a JSON object from OpenWeather
+
 // lat positive = North
 // lat negative = South
 // lon positive = West
 // lon negative = East
 // To DO : parse out NSEW for lon/lat
 
-var winky = [{
-  "coord": {
-    "lon": -122.09,
-    "lat": 37.39
-  },
-  "sys": {
-    "type": 3,
-    "id": 168940,
-    "message": 0.0297,
-    "country": "US",
-    "sunrise": 1427723751,
-    "sunset": 1427768967
-  },
-  "weather": [{
-    "id": 800,
-    "main": "Clear",
-    "description": "Sky is Clear",
-    "icon": "01n"
-  }],
-  "base": "stations",
-  "main": {
-    "temp": 285.68,
-    "humidity": 74,
-    "pressure": 1016.8,
-    "temp_min": 284.82,
-    "temp_max": 286.48
-  },
-  "wind": {
-    "speed": 0.96,
-    "deg": 285.001
-  },
-  "clouds": {
-    "all": 0
-  },
-  "dt": 1427700245,
-  "id": 0,
-  "name": "Mountain View",
-  "cod": 200
-}]
-var weather = {
-  location: "",
-  skies: "",
-  temp: 0,
-  windSpeed: 0,
-  windDirection: ""
-}
-
-function parseWinky() {
-  // function to parse a winky
-  // Make nicer strings
-  var latString = '';
-  var lonString = '';
-  if (winky[0]['coord'].lat<0){latString = "South"}
-  else {latString = "North"}
-  if (winky[0]['coord'].lon<0){lonString = "West"}
-  else {latString = "East"}
-
-  //weather.location = String(Math.abs(winky[0]['coord'].lat)) + ' degrees '+ latString + ' ' + String(Math.abs(winky[0]['coord'].lon)) + ' degrees ' + lonString;
-  weather.skies = winky[0]['weather'][0].description;
-  weather.temp = Math.floor(winky[0]['main'].temp-273.15);
-  weather.windSpeed = Math.round(winky[0]['wind'].speed);
-  weather.windDirection = String(Math.floor(winky[0]['wind'].deg));
-}
-
 //get IP-based location Object
 
-var json = (function () {
+var locJson = (function () {
     var json = null;
     $.ajax({
         'async': false,
@@ -85,17 +21,29 @@ var json = (function () {
     return json;
 })();
 
-console.log('DEBUG', json.city);
-// call OpenWeather with getIp['lat'], getIp['lon']
+/*
+console.log('DEBUG', locJson.city);
+console.log('DEBUG', locJson.lat);
+console.log('DEBUG', locJson.lon);
+*/
 
-var iconToDisplay = String(winky[0]['weather'][0].icon)
-console.log(iconToDisplay);
-var imageURL = 'http://openweathermap.org/img/w/'+iconToDisplay+'.png';
+// get weather at location
+
+var wJson = {};
+$.ajax(
+  {
+    url: "https://api.darksky.net/forecast/877c42bfced062f2b8e5cb98f65bb1ca/" + String(locJson.lat) + "," + String(locJson.lon),
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function(data) {
+      console.log(data);
+      wJson=data;
+    },
+    cache: false
+  });
+
+console.log(wJson);
+
+
 
  // call DOM manipulate icon URL
-
-
-
-
-parseWinky();
-console.log(weather.location, weather.skies, weather.temp, weather.windSpeed, weather.windDirection);
